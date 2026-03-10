ac28694da45978ebb330761fd168d0fa7e3ec00a
Brandon Barksdale <bbark93@gmail.com>
2026-03-10 11:09:30 -0400

    POST /orders

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -11,6 +11,68 @@ function list(req, res) {
   res.json({ data: orders });
 }
 
- module.exports = {
-   list,
- };
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
+}
+
+function dishesIsValid(req, res, next) {
+  const { data: { dishes } = {} } = req.body;
+
+  if (!dishes || !Array.isArray(dishes) || dishes.length === 0) {
+    return next({
+      status: 400,
+      message: "Order must include at least one dish",
+    });
+  }
+
+  for (let index = 0; index < dishes.length; index++) {
+    const { quantity } = dishes[index];
+
+    if (quantity === undefined || quantity <= 0) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is greater than 0`,
+      });
+    }
+
+    if (!Number.isInteger(quantity)) {
+      return next({
+        status: 400,
+        message: `dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+  }
+
+  next();
+}
+
+function create(req, res) {
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+  const newId = nextId();
+  const newOrder = {
+    id: newId,
+    deliverTo: deliverTo,
+    mobileNumber: mobileNumber,
+    status: status,
+    dishes: dishes,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+
+module.exports = {
+  list,
+  create: [
+    bodyDataHas("deliverTo"),
+    bodyDataHas("mobileNumber"),
+    bodyDataHas("dishes"),
+    dishesIsValid,
+    create,
+  ],
+};
